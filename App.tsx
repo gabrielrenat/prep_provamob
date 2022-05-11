f8161963a668ff6358aabc34d94affbf347e0d78
gabriel <gabrielrenatovga@yahoo.com>
2022-05-11 08:48:05 -0300

    prep_prova

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import "react-native-gesture-handler";
+import {ChatScreen} from './src/screens';
+
+
+export default function App() {
+  return <ChatScreen />;
+}
+
---
+++ src/screens/Chat/styles.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+import colors from "../../styles/colors";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 10,
+    alignItems: 'center',
+  },
+  rowSearch: {
+    flexDirection: 'row',
+    backgroundColor: colors.primaryLight,
+    borderRadius: 10,
+    margin: 10,
+    alignItems: 'center',
+    width: "80%",
+    height: 45,
+  },
+  input: {
+    width: "80%"
+  },
+  icon: {
+    fontSize: 24,
+    padding: 5
+  }
+})
+
+export default styles;
